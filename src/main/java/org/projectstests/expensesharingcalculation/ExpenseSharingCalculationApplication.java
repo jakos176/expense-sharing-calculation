0fe4321cd5c937099a1349d64069138657889163
Edu <edumartsil95@tutanota.com>
2022-05-04 14:07:00 +0200

    project files

---
+++ src/main/java/org/projectstests/expensesharingcalculation/ExpenseSharingCalculationApplication.java
@@ -0,0 +1,13 @@
+package org.projectstests.expensesharingcalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSharingCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseSharingCalculationApplication.class, args);
+	}
+
+}
